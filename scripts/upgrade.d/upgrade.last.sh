8277a5ef38f693e769fc996cb3e563164fb293ce
ljf (zamentur) <zamentur@users.noreply.github.com>
2023-11-23 01:03:22 +0100

    26.0.8

--- scripts/upgrade.d/upgrade.last.sh
+++ scripts/upgrade.d/upgrade.last.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Last available Nextcloud version
-next_version="26.0.7"
+next_version="26.0.8"
 
 # Nextcloud tarball checksum sha256
-nextcloud_source_sha256="bed244a8b94d13b616a92740521670c1d67d43c48047723fb13180bff6d48e92"
+nextcloud_source_sha256="c9d09acb446e33601960829133f8f376eb6f279a4def708217fc71581892887f"
